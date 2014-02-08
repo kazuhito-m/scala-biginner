dd8c8b64a6faf38f7b52913e05a21162b2d13ed2
kazuhito_m <kazuhito.sumpic@gmail.com>
2014-02-09 03:35:20 +0900

    rename sample file.

---
+++ src/main/scala/Sample001.scala
@@ -0,0 +1,31 @@
+import scala.io._
+
+
+object Sample001 {
+    def toInt(in: String): Option[Int] = 
+      try {
+        Some(Integer.parseInt(in.trim))
+      } catch {
+        case e: NumberFormatException => None
+      }
+    
+    
+    def sum(in: Seq[String]) = {
+      val ints = in.flatMap(s => toInt(s))
+      ints.foldLeft(0)((a,b) => a + b)
+    }
+
+    def main(s: Array[String]): Unit = {
+        println("Enter some numbers and press ctrl-D (Unix/Linux/Mac) ctrl-Z (windows)")
+        
+        val input = Source.fromInputStream(System.in)
+        
+        // XXX for old version Scala.
+        // val lines = input.getLines.collect
+        // now version Scala.
+        val lines = input.getLines.toList
+        
+        println("Sum " + sum(lines))
+    
+    }
+}

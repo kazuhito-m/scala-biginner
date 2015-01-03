fff8051ef01b19f61b32eeadad2c9ae8eb48ad1e
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-04 00:02:02 +0900

    コップ本の一つ目の例をテストに追加。

---
+++ src/test/scala/SampleTest.scala
@@ -0,0 +1,22 @@
+package test.scala
+
+import org.specs2.mutable._
+
+/**
+ * コップ本のサンプルを書く場所。
+ * (置くとこないので、ここに仮置き)
+ * Created by kazuhito on 15/01/03.
+ */
+class SampleTest extends Specification {
+
+  "本の序盤に載ってるサンプル" should {
+    "1_1_マップの例" in {
+      var capital = Map("US" -> "Washington" , "France" -> "Paris")
+      capital += ("Japan" -> "Tokyo")
+      println(capital("France"))
+      capital.size must equalTo(3)
+    }
+
+  }
+
+}

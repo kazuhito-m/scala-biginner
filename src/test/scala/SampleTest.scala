0586aa0f478e1b2ba5f9f36cdcdecc41e9cceeed
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-06 00:52:01 +0900

    1.1.1のBigIntの例を追加。

--- src/test/scala/SampleTest.scala
+++ src/test/scala/SampleTest.scala
@@ -11,11 +11,18 @@ class SampleTest extends Specification {
 
   "本の序盤に載ってるサンプル" should {
     "1_1_マップの例" in {
-      var capital = Map("US" -> "Washington" , "France" -> "Paris")
+      var capital = Map("US" -> "Washington", "France" -> "Paris")
       capital += ("Japan" -> "Tokyo")
       println(capital("France"))
       capital.size must equalTo(3)
       capital("France") must equalTo("Paris")
+    }
+
+    "1_1_1_BigIntの例" in {
+      def factorial(x: BigInt): BigInt =
+        if (x == 0) 1 else x * factorial(x - 1)
+      val actual = factorial(30)
+      actual.toString() must equalTo("265252859812191058636308480000000")
     }
 
   }

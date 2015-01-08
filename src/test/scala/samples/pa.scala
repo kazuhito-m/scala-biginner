87ad2db7e3464b47755097bf5e01f9bac3f657b5
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-09 01:39:14 +0900

    コップ本2.6の例で更なる略記方法を追加。

--- src/test/scala/samples/pa.scala
+++ src/test/scala/samples/pa.scala
@@ -1,4 +1,7 @@
 // コップ本2.6の例。
 // args.foreach(arg => println(arg))
 // きっちり書いた例
-args.foreach((arg: String) => println(arg))
+// args.foreach((arg: String) => println(arg))
+// もっと簡易(特別な略記法)に書いた例。
+args.foreach(println)
+

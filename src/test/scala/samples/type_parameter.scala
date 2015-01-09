29b4d47ed5b968b13ab8067d88540ea4a1449262
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-10 04:48:12 +0900

    配列のパラメータとして型を指定する、例を追加。

--- src/test/scala/samples/type_parameter.scala
+++ src/test/scala/samples/type_parameter.scala
@@ -1,2 +1,13 @@
+// コンストラクタに型パラメータを渡す例。
 val big = new java.math.BigInteger("12345")
 println(big)
+
+// 配列のパラメータとして型を指定する例
+val greetStrings = new Array[String](3)
+
+greetStrings(0) = "Hello"
+greetStrings(1) = ", " 
+greetStrings(2) = "world!\n"  
+
+for (i <- 0 to 2)
+    print(greetStrings(i))

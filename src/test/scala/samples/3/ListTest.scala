a719775a4daf9817ffbb2ce9f4a2ce9e038a9406
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-01-11 10:32:12 +0900

    微調整。リフォーマット。

--- src/test/scala/samples/3/ListTest.scala
+++ src/test/scala/samples/3/ListTest.scala
@@ -6,19 +6,20 @@ import org.specs2.mutable._
 class ListTest extends Specification {
 
   "コップ本「3.2リストを使う」に載ってるサンプル" should {
+
     "単純なリストの例" in {
-      val oneTwoThree = List(1,2,3)
+      val oneTwoThree = List(1, 2, 3)
       oneTwoThree.size must equalTo(3)
     }
 
     "リスト同士の連結" in {
-      val oneTwo = List(1,2)
-      val threeFour = List(3,4)
+      val oneTwo = List(1, 2)
+      val threeFour = List(3, 4)
       val oneTwoThreeFour = oneTwo ::: threeFour
       println("" + oneTwo + " and " + threeFour + " were not mutated.")
       println("Thus, " + oneTwoThreeFour + " is a new list.")
 
-      val expected = List(1,2,3,4)
+      val expected = List(1, 2, 3, 4)
       oneTwoThreeFour must equalTo(expected)
     }
 

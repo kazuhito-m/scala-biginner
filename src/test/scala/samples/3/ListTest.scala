932d6b79a31c1e4d98466c91215ba435ec628e35
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-01-11 10:29:43 +0900

    3.2のサンプルの二例目追加。

--- src/test/scala/samples/3/ListTest.scala
+++ src/test/scala/samples/3/ListTest.scala
@@ -11,6 +11,17 @@ class ListTest extends Specification {
       oneTwoThree.size must equalTo(3)
     }
 
+    "リスト同士の連結" in {
+      val oneTwo = List(1,2)
+      val threeFour = List(3,4)
+      val oneTwoThreeFour = oneTwo ::: threeFour
+      println("" + oneTwo + " and " + threeFour + " were not mutated.")
+      println("Thus, " + oneTwoThreeFour + " is a new list.")
+
+      val expected = List(1,2,3,4)
+      oneTwoThreeFour must equalTo(expected)
+    }
+
   }
 }
 

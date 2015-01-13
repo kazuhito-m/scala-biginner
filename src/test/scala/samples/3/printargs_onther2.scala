3f150b4ca602cacef8356226dd3a509cffee6ad6
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-14 02:52:45 +0900

    ワンライナーに出来て副作用が無い関数になったら、テスト可能なのでテスト書いてみる。

--- src/test/scala/samples/3/printargs_onther2.scala
+++ src/test/scala/samples/3/printargs_onther2.scala
@@ -1,9 +1,24 @@
+import org.specs2.mutable.Specification
+
 /**
  * コンソール引数に指定された文字を、開業しながら出す…の関数型ワンライナー版。
  */
 object printaargs_other2 {
   def formatArgs(args: Array[String]) = args.mkString("\n")
+
   def main(args: Array[String]): Unit = {
-    println(formatArgs(args))  
+    println(formatArgs(args))
   }
 }
+
+class PrintArgsTest extends Specification {
+
+  "コップ本「3.5関数型のスタイルが見分ける」に載ってるサンプル" should {
+    "関数型アプローチになるとテストできるのでテストする" in {
+      val param = Array("1", "2", "3", "end")
+      printaargs_other2.formatArgs(param) must equalTo("1\n2\n3\nend")
+    }
+  }
+
+}
+

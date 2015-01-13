3d5567c1d2f936a2b6c97586ea9e948587d9f825
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-14 02:43:41 +0900

    オブジェクト名が間違ってたので、修正。

--- src/test/scala/samples/3/printargs_onther.scala
+++ src/test/scala/samples/3/printargs_onther.scala
@@ -1,7 +1,7 @@
 /**
  * コンソール引数に指定された文字を、開業しながら出す…の命令形（非関数型）版。
  */
-object type_parameter {
+object printaargs_other {
   def main(args: Array[String]): Unit = {
     var i = 0
     while (i < args.length) {
--- src/test/scala/samples/3/printargs_onther2.scala
+++ src/test/scala/samples/3/printargs_onther2.scala
@@ -1,7 +1,7 @@
 /**
  * コンソール引数に指定された文字を、開業しながら出す…の関数型ワンライナー版。
  */
-object type_parameter {
+object printaargs_other2 {
   def formatArgs(args: Array[String]) = args.mkString("\n")
   def main(args: Array[String]): Unit = {
     println(formatArgs(args))  

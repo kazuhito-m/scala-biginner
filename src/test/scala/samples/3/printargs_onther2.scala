343e7f4498fd955b05a4a72650b55539794394a2
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-14 02:57:07 +0900

    またもスペルミスしてたので修正。

--- src/test/scala/samples/3/printargs_onther2.scala
+++ src/test/scala/samples/3/printargs_onther2.scala
@@ -3,7 +3,7 @@ import org.specs2.mutable.Specification
 /**
  * コンソール引数に指定された文字を、開業しながら出す…の関数型ワンライナー版。
  */
-object printaargs_other2 {
+object printargs_other2 {
   def formatArgs(args: Array[String]) = args.mkString("\n")
 
   def main(args: Array[String]): Unit = {
@@ -16,7 +16,7 @@ class PrintArgsTest extends Specification {
   "コップ本「3.5関数型のスタイルが見分ける」に載ってるサンプル" should {
     "関数型アプローチになるとテストできるのでテストする" in {
       val param = Array("1", "2", "3", "end")
-      printaargs_other2.formatArgs(param) must equalTo("1\n2\n3\nend")
+      printargs_other2.formatArgs(param) must equalTo("1\n2\n3\nend")
     }
   }
 

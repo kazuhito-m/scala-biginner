c6219a3b426a44cb80ddc256a1377ffad67ee716
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-16 01:43:49 +0900

    微調整。テスト量産するために、クラスをテストケース内に移動。

--- src/test/scala/samples/4/ClassAndObjectTest.scala
+++ src/test/scala/samples/4/ClassAndObjectTest.scala
@@ -7,18 +7,25 @@ import org.specs2.mutable.Specification
  */
 class ClassAndObjectTest extends Specification {
 
-  class ChecksumAccumlator {
-    var sum = 0
-  }
-
   "オブジェクトとその上に乗っているフィールドは編集可不可が違う" should {
 
     "オブジェクト二つ作って比較" in {
+
+      class ChecksumAccumlator {
+        var sum = 0
+      }
+
       val acc = new ChecksumAccumlator()
       val sca = new ChecksumAccumlator()
 
       // 作成直後は値は一緒。
       acc.sum must equalTo(sca.sum)
+
+      // 値を変更することはできて、
+      sca.sum = 3
+
+      // それは参照したときに違うもの。
+      acc.sum must not equalTo(sca.sum)
 
     }
 

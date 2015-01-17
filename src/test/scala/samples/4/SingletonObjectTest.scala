f86c80a346ddb2d80b9cc967e326b95e8d4f2a68
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-18 00:05:12 +0900

    微調整。ソースフォーマット。

--- src/test/scala/samples/4/SingletonObjectTest.scala
+++ src/test/scala/samples/4/SingletonObjectTest.scala
@@ -7,16 +7,16 @@ import org.specs2.mutable.Specification
  */
 class SingletonObjectTest extends Specification {
 
-    "ChecksumAccumlatorクラスとChecksumAccumlatorシングルトンオブジェクト" in {
+  "ChecksumAccumlatorクラスとChecksumAccumlatorシングルトンオブジェクト" in {
 
-      // まず一度目。
-      val checkTarget = "Every value is an object."
-      ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
+    // まず一度目。
+    val checkTarget = "Every value is an object."
+    ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
 
-      // それ以降も同じ値を返す。（キャッシュされているからそっから結果を出している…はず)
-      ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
-      ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
-
-    }
+    // それ以降も同じ値を返す。（キャッシュされているからそっから結果を出している…はず)
+    ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
+    ChecksumAccumlator.calculate(checkTarget) must equalTo(-248)
 
   }
+
+}

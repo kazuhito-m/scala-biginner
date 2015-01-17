06021f4cad7e954a13e48a95544c6247f405fb1c
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-18 00:29:02 +0900

    Summerオブジェクトのファイルを追加。

---
+++ src/test/scala/samples/4/Summer.scala
@@ -0,0 +1,12 @@
+package samples
+
+import ChecksumAccumlator.calculate
+
+object Summer {
+
+  def main(args: Array[String]):Unit = {
+    for (arg <- args)
+      println(arg + ": " + calculate(arg))
+  }
+
+}

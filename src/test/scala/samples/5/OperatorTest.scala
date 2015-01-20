1863927ff40115846ec2c540eac337811a6e0f9e
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-21 02:19:23 +0900

    Intの除算と剰余算を追加。

--- src/test/scala/samples/5/OperatorTest.scala
+++ src/test/scala/samples/5/OperatorTest.scala
@@ -13,4 +13,20 @@ class OperatorTest  extends Specification {
     val value = 3 - 1
     value must equalTo(Int.box(2))
   }
+
+  "Longになる場合" in {
+    val value = 2L * 3L
+    value must equalTo(Long.box(6L))
+  }
+
+  "Intで除算の場合" in {
+    val value = 11 / 4
+    value must equalTo(Int.box(2))
+  }
+
+  "Intで剰余算の場合" in {
+    val value = 11 % 4
+    value must equalTo(Int.box(3))
+  }
+
 }

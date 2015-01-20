0d7c52255d49a37d4fce0efc8bb38d6831d41cf1
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-21 02:29:19 +0900

    テストの基本形を忘れていた。Should節で囲む。

--- src/test/scala/samples/5/OperatorTest.scala
+++ src/test/scala/samples/5/OperatorTest.scala
@@ -2,41 +2,45 @@ package samples
 
 import org.specs2.mutable.Specification
 
-class OperatorTest  extends Specification {
+class OperatorTest extends Specification {
 
-  "Doubleに成る場合" in {
-    val value = 1.2 + 2.3
-    value must equalTo(Double.box(3.5d))
-  }
+  "演算子とその演算で決まる左辺型" should {
 
-  "Intに成る場合" in {
-    val value = 3 - 1
-    value must equalTo(Int.box(2))
-  }
+    "Doubleに成る場合" in {
+      val value = 1.2 + 2.3
+      value must equalTo(Double.box(3.5d))
+    }
 
-  "Longになる場合" in {
-    val value = 2L * 3L
-    value must equalTo(Long.box(6L))
-  }
+    "Intに成る場合" in {
+      val value = 3 - 1
+      value must equalTo(Int.box(2))
+    }
 
-  "Intで除算の場合" in {
-    val value = 11 / 4
-    value must equalTo(Int.box(2))
-  }
+    "Longになる場合" in {
+      val value = 2L * 3L
+      value must equalTo(Long.box(6L))
+    }
 
-  "Intで剰余算の場合" in {
-    val value = 11 % 4
-    value must equalTo(Int.box(3))
-  }
+    "Intで除算の場合" in {
+      val value = 11 / 4
+      value must equalTo(Int.box(2))
+    }
 
-  "Floatの除算の場合" in {
-    val value = 11.0f / 4.0f
-    value must equalTo(Float.box(2.75f))
-  }
+    "Intで剰余算の場合" in {
+      val value = 11 % 4
+      value must equalTo(Int.box(3))
+    }
 
-  "小数点の除算だがDoubleになる場合" in {
-    val value = 11.0 % 4.0
-    value must equalTo(Double.box(3.0))
+    "Floatの除算の場合" in {
+      val value = 11.0f / 4.0f
+      value must equalTo(Float.box(2.75f))
+    }
+
+    "小数点の除算だがDoubleになる場合" in {
+      val value = 11.0 % 4.0
+      value must equalTo(Double.box(3.0))
+    }
+
   }
 
 }

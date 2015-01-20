7eddbc144e5e26cf7172ce43b7b9073ac0b4857d
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-01-20 22:46:01 +0900

    プリミティブ型の解説している章のサンプルを追加。

---
+++ src/test/scala/samples/5/PrimitiveTest.scala
@@ -0,0 +1,16 @@
+package samples
+
+import org.specs2.mutable.Specification
+
+class PrimitiveTest extends Specification {
+
+  "プリミティブ型のメソッドの扱いについての色々" in {
+
+    // 中置演算子はメソッドなので、メソッド型としても書ける。
+    val value1 = 1 * 3
+    val value2 = (1).*(3)
+
+    value1 must equalTo(value2)
+
+  }
+}

8bac93465ce146a833fe57cc45aa3afa59c03e24
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-22 00:58:20 +0900

    ビット演算例を追加。

--- src/test/scala/samples/5/LogicalOperatorTest.scala
+++ src/test/scala/samples/5/LogicalOperatorTest.scala
@@ -65,6 +65,9 @@ class LogicalOperatorTest extends Specification {
 
     "ビット単位の比較演算" in {
       1 & 2 must equalTo(Int.box(0))
+      1 | 2 must equalTo(Int.box(3))
+      1 ^ 3 must equalTo(Int.box(2))
+      ~1 must equalTo(Int.box(-2))
     }
 
   }

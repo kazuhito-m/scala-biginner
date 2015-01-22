57b4aea8f8284ae7990d58a260f0277b09ae481a
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-23 02:19:12 +0900

    5.8の例となるクラスファイルを追加。

---
+++ src/test/scala/samples/5/OperatorsPrecedenceTest.scala
@@ -0,0 +1,36 @@
+package samples
+
+import org.specs2.mutable.Specification
+
+class EqualOperatorTest extends Specification {
+
+  "オブジェクトの等価性" should {
+
+    "等号と不等号" in {
+      1 == 2 must equalTo(false)
+      1 != 2 must equalTo(true)
+      2 == 2 must equalTo(true)
+    }
+
+    "オブジェクトの比較での等号" in {
+      List(1, 2, 3) == List(4, 5, 6) must equalTo(false)
+      List(1, 2, 3) == List(1, 2, 3) must equalTo(true)
+    }
+
+    "型の違うオブジェクトでも比較が可能" in {
+      1L == 1.0D must equalTo(true)
+      List(1, 2, 3) == "文字" must equalTo(false)
+    }
+
+    "nullの比較も可能" in {
+      List(1, 2, 3) == null must equalTo(false)
+      null == List(1, 2, 3) must equalTo(false)
+    }
+
+    "参照でなく等価かをチェックする例(nullチェック不要)" in {
+      ("he" + "llo") == "hello" must equalTo(true)
+    }
+
+  }
+
+}

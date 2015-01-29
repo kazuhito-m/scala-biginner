14236bb66b8e1fadf70c98b83fb0e8dc328f4158
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-30 02:17:11 +0900

    等式推論の例を追加。

--- src/test/scala/samples/7/BuiltinControlStructuresTest.scala
+++ src/test/scala/samples/7/BuiltinControlStructuresTest.scala
@@ -7,7 +7,7 @@ class BuiltinControlStructuresTest extends Specification {
 
   "組み込み制御構造" should {
 
-    "if式（7.1の内容）" in {
+    "条件if式(7.1の内容)" in {
       val args: Array[String] = Array()
       // 命令形スタイル
       var filename1 = "default.txt"
@@ -15,6 +15,8 @@ class BuiltinControlStructuresTest extends Specification {
         filename1 = args(0)
       // 関数型スタイル
       val filename2 = if (!args.isEmpty) args(0) else "default.txt"
+      // 等式推論例
+      println(if (!args.isEmpty) args(0) else "default.txt")
       // 書き方違えど双方結果は同じ
       filename1 must equalTo(filename2)
     }

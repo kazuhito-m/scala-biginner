24dd675b05127e24898692338b9aa54b5fa1c006
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-01-30 02:25:46 +0900

    Whileループ例を追加。

--- src/test/scala/samples/7/BuiltinControlStructuresTest.scala
+++ src/test/scala/samples/7/BuiltinControlStructuresTest.scala
@@ -21,6 +21,32 @@ class BuiltinControlStructuresTest extends Specification {
       filename1 must equalTo(filename2)
     }
 
+    "whileループ(7.2の内容)" in {
+
+      // 命令型スタイルの最大公約数計算
+      def gcdLoop(x:Long, y:Long): Long = {
+        var a = x
+        var b = y
+        while (a != 0) {
+          val temp = a
+          a = b % a
+          b = temp
+        }
+        b
+      }
+
+      // 関数型スタイルの最大公約数計算
+      def gcd(x: Long, y: Long): Long = {
+        if (y == 0) x else gcd(y , x % y)
+      }
+
+      val i = 9
+      val j = 6
+
+      gcdLoop(i,j) must equalTo(gcd(i,j))
+
+    }
+
   }
 
 }

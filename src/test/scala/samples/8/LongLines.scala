199f1c2880ec75725e605edb8ec2b1df1315f447
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-05 03:32:49 +0900

    リファクタリング。不要な引数（内側だから変数が使えるので不要）を削除。

--- src/test/scala/samples/8/LongLines.scala
+++ src/test/scala/samples/8/LongLines.scala
@@ -4,14 +4,14 @@ object LongLines {
 
   def processFile(filename: String, width: Int) {
 
-    def processLine(filename: String, width: Int, line: String) {
+    def processLine(line: String) {
       if (line.length < width)
         println(filename + ": " + line.trim)
     }
 
     val source = Source.fromFile(filename)
     for (line <- source.getLines)
-      processLine(filename, width, line)
+      processLine(line)
   }
 
 }

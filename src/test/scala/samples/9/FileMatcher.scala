08d2a7945a2e19bbff4d1036007cc77416ecaea2
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-10 03:49:32 +0900

    リファクタリング。省略できる点があり変更。

--- src/test/scala/samples/9/FileMatcher.scala
+++ src/test/scala/samples/9/FileMatcher.scala
@@ -6,16 +6,16 @@ object FileMatcher {
 
   private def filesHere = (new File(".")).listFiles
 
-  def filesMatching(query: String, matcher: (String, String) => Boolean) = {
-    for (file <- filesHere; if matcher(file.getName, query))
+  def filesMatching(query: String, matcher: (String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName))
       yield file
   }
 
-  def fileEnding(query: String) = filesMatching(query, _.endsWith(_))
+  def fileEnding(query: String) = filesMatching(query, _.endsWith(query))
 
-  def filesContaining(query: String) = filesMatching(query , _.contains(_))
+  def filesContaining(query: String) = filesMatching(query , _.contains(query))
 
-  def filesRegex(query: String) = filesMatching(query, _.matches(_))
+  def filesRegex(query: String) = filesMatching(query, _.matches(query))
 
 }
 

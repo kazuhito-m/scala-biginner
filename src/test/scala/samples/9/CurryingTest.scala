4c0aefbbf3e9a749b99d333e14fd09bf51e948a4
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-11 04:09:51 +0900

    existsを使う別のメソッドとテストも追加。

--- src/test/scala/samples/9/CurryingTest.scala
+++ src/test/scala/samples/9/CurryingTest.scala
@@ -1,10 +1,6 @@
 package samples
 
 import org.specs2.mutable.Specification
-import java.io.{IOException, FileNotFoundException, File}
-import java.io
-import java.net.{MalformedURLException, URL}
-import scalaz.Digit._0
 
 class CurryingTest extends Specification {
 
@@ -34,6 +30,33 @@ class CurryingTest extends Specification {
 
       containsNegC(baseParam) must equalTo(expected)
       containsNegF(baseParam) must equalTo(expected)
+
+    }
+
+    // 命令形で書いたList内に奇数があるか検索
+    def containsOddC(nums: List[Int]): Boolean = {
+      var exists = false
+      for (num <- nums)
+        if (num % 2 == 1)
+          exists = true
+      exists
+    }
+
+    // 関数型で書いたList内に奇数があるか検索
+    def containsOddF(nums: List[Int]): Boolean = nums.exists(_ % 2 == 1)
+
+    "奇数検出の関数のテスト" in {
+      var baseParam = List(2, 4, 10)
+      var expected = false
+
+      containsOddC(baseParam) must equalTo(expected)
+      containsOddF(baseParam) must equalTo(expected)
+
+      baseParam = List(2, 3, 4)
+      expected = true
+
+      containsOddC(baseParam) must equalTo(expected)
+      containsOddF(baseParam) must equalTo(expected)
 
     }
 

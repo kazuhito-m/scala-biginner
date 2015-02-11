22e2b1632fc360522569387f172cac9529373ca8
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-12 04:07:28 +0900

    ファイルのクローズなど、前後処理を行う構造を定義する。

--- src/test/scala/samples/9/UserDefinitionControlStructureTest.scala
+++ src/test/scala/samples/9/UserDefinitionControlStructureTest.scala
@@ -1,6 +1,8 @@
 package samples
 
 import org.specs2.mutable.Specification
+import java.io.{PrintWriter, File}
+import scala.io.Source
 
 class UserDefinitionControlStructureTest extends Specification {
 
@@ -14,6 +16,29 @@ class UserDefinitionControlStructureTest extends Specification {
       val actual = twice(_ + 1, 5)
 
       actual must equalTo(7)
+
+    }
+
+    "初期処理と終了処理を面倒見る制御構造を作る" in {
+
+      def withPrintWriter(file: File, op: PrintWriter => Unit) {
+        val writer = new PrintWriter(file)
+        try {
+          op(writer)
+        } finally {
+          writer.close()
+        }
+      }
+
+      withPrintWriter(
+        new File("test.txt"),
+        writer => writer.println("Test text.")
+      )
+
+      val actual = Source.fromFile("test.txt").getLines.reduceLeft(_ + _)
+      (new File("test.txt")).delete
+
+      actual must equalTo("Test text.")
 
     }
 

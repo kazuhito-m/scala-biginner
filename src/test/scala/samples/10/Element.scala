d6d91956c9d69c3b2538311c763a7ea5dcbec7b3
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-14 23:48:31 +0900

    Elementクラスにパラメータ無しのメソッドをふたつ追加。

--- src/test/scala/samples/10/Element.scala
+++ src/test/scala/samples/10/Element.scala
@@ -2,6 +2,12 @@ package samples
 
 
 abstract class Element {
+
   def contents: Array[String]
+
+  def height: Int = contents.length
+
+  def width: Int = if (height == 0) 0 else contents(0).length
+
 }
 

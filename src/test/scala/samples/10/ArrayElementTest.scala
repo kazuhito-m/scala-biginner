8fc3912a94b74ad6c1d196dfb8c23db7d60d8f3b
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-02-15 02:15:43 +0900

    新規クラスをGit的に追加漏れしてたので、改て追加。

--- src/test/scala/samples/10/ArrayElementTest.scala
+++ src/test/scala/samples/10/ArrayElementTest.scala
@@ -7,11 +7,13 @@ class ArrayElementTest  extends Specification {
   "クラスの拡張(10.4)" should {
 
     "継承のテスト" in {
-
       val ae = new ArrayElement(Array("Hello","World"))
-
       ae.width must equalTo(5)
+    }
 
+    "必要に応じて継承先のクラスの値が参照される" in {
+      val e: Element = new ArrayElement(Array("Hello"))
+      e.width must equalTo(5)
     }
 
   }

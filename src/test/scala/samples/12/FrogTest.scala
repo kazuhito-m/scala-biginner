494d62f08419435f57482c13b145c4bc69ab6773
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-02 01:21:33 +0900

    トレイトの継承の例追加。

--- src/test/scala/samples/12/FrogTest.scala
+++ src/test/scala/samples/12/FrogTest.scala
@@ -4,4 +4,11 @@ import org.specs2.mutable.Specification
 
 class FrogTest  extends Specification {
 
+  "トレイトの仕組み(12.1)" should {
+    "トレイトのExtends時の動き" in {
+      val frog = new Flog
+      frog.toString must equalTo("green")
+    }
+  }
+
 }

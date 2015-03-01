2a46542a44fe1cc123cb9e4266658df0b2cff183
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-02 01:32:48 +0900

    微調整。改行整え。

--- src/test/scala/samples/12/FrogTest.scala
+++ src/test/scala/samples/12/FrogTest.scala
@@ -5,6 +5,7 @@ import org.specs2.mutable.Specification
 class FrogTest extends Specification {
 
   "トレイトの仕組み(12.1)" should {
+
     "トレイトのExtends時の動き" in {
       val frog = new Flog
       // 継承元トレイトのメソッドも呼べるし
@@ -18,6 +19,7 @@ class FrogTest extends Specification {
       // 同じメソッドをよんでも「インスタンス側の性質」で呼び出される。
       phil.toString must equalTo("green")
     }
+
   }
 
 }

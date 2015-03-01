3aac04834bb3dbe1f12e3ce1334288a4ad352be1
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-02 01:23:48 +0900

    継承元のトレイトの機能が呼べることを確認。

--- src/test/scala/samples/12/FrogTest.scala
+++ src/test/scala/samples/12/FrogTest.scala
@@ -2,11 +2,14 @@ package samples
 
 import org.specs2.mutable.Specification
 
-class FrogTest  extends Specification {
+class FrogTest extends Specification {
 
   "トレイトの仕組み(12.1)" should {
     "トレイトのExtends時の動き" in {
       val frog = new Flog
+      // 継承元トレイトのメソッドも呼べるし
+      frog.philosophize()
+      // 勿論「自身クラスの特徴」も保持している
       frog.toString must equalTo("green")
     }
   }

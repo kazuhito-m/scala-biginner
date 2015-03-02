77ba859c3b20e75d264193736cf166729edb15e4
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-03 01:20:42 +0900

    微調整。利フォーマット。1

--- src/test/scala/samples/12/Rational2Test.scala
+++ src/test/scala/samples/12/Rational2Test.scala
@@ -8,7 +8,7 @@ class Rational2Test extends Specification {
 
     "Orderdトレイトを装備するだけで比較が可能になる" in {
       val half = new Rational2(1, 2)
-      val third = new Rational2(1,3)
+      val third = new Rational2(1, 3)
 
       half < third must equalTo(false)
       half > third must equalTo(true)

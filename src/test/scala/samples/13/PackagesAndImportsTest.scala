4226075e29f6c87a97198a44cf567fbd87987214
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-09 03:38:43 +0900

    分岐が出来るようなパッケージ階層の例を追加。

--- src/test/scala/samples/13/PackagesAndImportsTest.scala
+++ src/test/scala/samples/13/PackagesAndImportsTest.scala
@@ -3,12 +3,18 @@ package samples
 import org.specs2.mutable.Specification
 import samples.bobsrockets.navigation.Navigator
 import samples.bobsrockets.navigation.tests.NavigatorSuite
+import samples.bobsrockets.launch.Booster
 
 package bobsrockets {
   package navigation {
     class Navigator
     package tests{
       class NavigatorSuite
+    }
+  }
+  package launch {
+    class Booster {
+      val nav = new navigation.Navigator
     }
   }
 }
@@ -25,6 +31,11 @@ class PackagesAndImportsTest extends Specification {
       sut2.getClass.getPackage.getName must equalTo("samples.bobsrockets.navigation.tests")
     }
 
+  }
+
+  "Scalaパッケージは本当に入れ子になる" in {
+    val sut = new Booster
+    sut.getClass.getPackage.getName must equalTo("samples.bobsrockets.launch")
   }
 
 }

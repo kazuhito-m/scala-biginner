4e9a1dd6185b449b22a8675f1324349e01bcc85d
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-03-11 02:22:06 +0900

    本の通り書いたのにうまくいかない。コンパイルエラーにナリたくないのでコメントアウト。

--- src/test/scala/samples/13/PackagesAndImportsTest.scala
+++ src/test/scala/samples/13/PackagesAndImportsTest.scala
@@ -11,12 +11,26 @@ package bobsrockets {
     package tests{
       class NavigatorSuite
     }
+    package launch {
+      class Booster1
+    }
+    class MissionControl {
+      val booster1 = new launch.Booster1
+      val booster2 = new bobsrockets.launch.Booster2
+      // FIXME コレがなぜか通らない。コメントアウト
+      // val booster3 = new _root_.launch.Booster3
+    }
   }
   package launch {
     class Booster {
       val nav = new navigation.Navigator
     }
+    class Booster2
   }
+
+}
+package launch {
+  class Booster3
 }
 
 

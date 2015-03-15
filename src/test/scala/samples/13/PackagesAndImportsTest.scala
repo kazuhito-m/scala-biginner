970f719e138683dca70f603453f59f33cd21618a
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-03-15 23:20:03 +0900

    テスト名がおかしった

--- src/test/scala/samples/13/PackagesAndImportsTest.scala
+++ src/test/scala/samples/13/PackagesAndImportsTest.scala
@@ -53,7 +53,7 @@ class PackagesAndImportsTest extends Specification {
     sut.nav.getClass.getPackage.getName must equalTo("samples.bobsrockets.navigation")
   }
 
-  "インポート(13.2" should {
+  "インポート(13.2)" should {
     "関数の途中でインポートする例" in {
       import java.util.regex
       val hittest = "abcdef"
